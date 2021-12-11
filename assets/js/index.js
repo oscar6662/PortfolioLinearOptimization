d983c4acc5af3b20428f6783532520c8a6daee2f
oscar6662 <ohb5@hi.is>
2021-12-11 00:21:44 +0000

    local storage flow working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,22 @@
 import './Assets/modal.js';
 
 function render() {
-  if (window.localStorage.getItem('portfolio') === null) {
-    const json = '{items: []}';
-      window.localStorage.setItem('portfolio', JSON.stringify(json));
+  const data = JSON.parse(window.localStorage.getItem('portfolio'));
+  if (data === null) {
+    const json = { items: [] };
+    window.localStorage.setItem('portfolio', JSON.stringify(json));
     return;
   }
-  console.log(JSON.parse(window.localStorage.getItem('portfolio')));
+  console.log( data);
+  const mystuff = document.querySelector('.mystuff');
+  console.log(data.items[0]);
+  for(let item in data.items) {
+    console.log(item);
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${data.items[item].currency}, ${data.items[item].amount}`));
+    li.setAttribute("id", "element4");
+    mystuff.appendChild(li);
+  }
 }
 
 function addNewPurchase() {
@@ -16,9 +26,9 @@ function addNewPurchase() {
     alert('Error. Invalid Input');
     return;
   }
-  const data = JSON.parse(window.localStorage.getItem('portfolio'));
+  let data = JSON.parse(window.localStorage.getItem('portfolio'));
   console.log(data);
-  data.items.push(`{"currency":${currency}, "amount":${amount}}`);
+  data.items.push({ "currency": currency, "amount": amount });
   window.localStorage.setItem('portfolio', JSON.stringify(data));
 }
 document.querySelector('.addNewPurchase').addEventListener('click', () => addNewPurchase());
