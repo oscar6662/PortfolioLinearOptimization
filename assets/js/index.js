3fd48668dde48e9e94866acea53cdf79bac57a47
oscar6662 <ohb5@hi.is>
2021-12-11 01:02:39 +0000

    linearOptimization added

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,17 +7,42 @@ function render() {
     window.localStorage.setItem('portfolio', JSON.stringify(json));
     return;
   }
-  console.log( data);
   const mystuff = document.querySelector('.mystuff');
-  console.log(data.items[0]);
   for(let item in data.items) {
-    console.log(item);
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(`${data.items[item].currency}, ${data.items[item].amount}`));
     li.setAttribute("id", "element4");
     mystuff.appendChild(li);
   }
 }
+
+
+let  model = {
+    "optimize": "capacity",
+    "opType": "max",
+    "constraints": {
+        "plane": {"max": 44},
+        "person": {"max": 512},
+        "cost": {"max": 300000}
+    },
+    "variables": {
+        "brit": {
+            "capacity": 20000,
+            "plane": 1,
+            "person": 8,
+            "cost": 5000
+        },
+        "yank": {
+            "capacity": 30000,
+            "plane": 1,
+            "person": 16,
+            "cost": 9000
+        }
+    },
+};
+ 
+const results = solver.Solve(model);
+console.log(results);
 
 function addNewPurchase() {
   const currency = document.querySelector('.currency').value;
